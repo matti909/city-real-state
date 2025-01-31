838d8bef6c24bed0e41a100d9dd82c7bb5f77aef
matti909 <matias.saantiago@gmail.com>
2025-01-31 10:38:27 -0300

    layout setup done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { AppLayout } from './layout/component/app.layout';
+import { MainComponent } from './components/main/main.component';
+import { LoginComponent } from './pages/login/login.component';
+export const appRoutes: Routes = [
+  {
+    path: '',
+    component: LoginComponent,
+  },
+  {
+    path: 'dashboard',
+    component: AppLayout,
+    children: [{ path: 'main', component: MainComponent }],
+  },
+];
---
+++ src/app/layout/component/app.footer.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-footer',
+  template: `<div class="layout-footer">
+    by
+    <span>M.T</span>
+  </div>`,
+})
+export class AppFooter {}
